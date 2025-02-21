7587aff9cc012d2097baa70d201ad5a86d32026f
Alexandra <melexandra98@gmail.com>
2025-02-21 16:03:51 -0300

    boton de prueba

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test Page</title>
+</head>
+<body>
+  <h1>This is a test</h1>
+  <button onclick="window.location.href='https://github.com/AlexBugsHunter/QA-Portfolio/tree/QA-Portfolio';">
+    See portfolio
+  </button>
+</body>
+</html>
