15ca3babde6e22eeee049a906799a46011f1fb39
Alexandra <melexandra98@gmail.com>
2025-02-21 16:11:02 -0300

    prueba boton n2

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Test Page</title>
+  <script>
+    function redirigir() {
+      window.location.href = 'https://github.com/AlexBugsHunter/QA-Portfolio/tree/QA-Portfolio';
+    }
+  </script>
 </head>
 <body>
   <h1>This is a test</h1>
-  <button onclick="window.location.href='https://github.com/AlexBugsHunter/QA-Portfolio/tree/QA-Portfolio';">
-    See portfolio
-  </button>
+  <button onclick="redirigir()">See portfolio</button>
 </body>
 </html>
